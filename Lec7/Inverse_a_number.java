0336a12b402612ee45f71252175c2f97ad387a5b
Ajaykush21 <ajaykushwaha7310@gmail.com>
2023-08-23 14:10:09 +0530

    Qps

---
+++ Lec7/Inverse_a_number.java
@@ -0,0 +1,23 @@
+package Lec7;
+import java.util.*;
+public class Inverse_a_number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		int pos=1;
+		while(n>0) {
+			int rem=n%10;
+			 sum=(int)(sum+pos*Math.pow(10, rem-1));//sum=sum+pos*(10^rem-1);
+			n/=10;
+			pos++;
+			
+		}
+		System.out.print(sum);
+		
+
+	}
+
+}
