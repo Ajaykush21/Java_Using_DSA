ac49ccda811d5a8e6272a61f8a4ca22a077c71df
Ajaykush21 <ajaykushwaha7310@gmail.com>
2023-08-24 14:20:50 +0530

    Function

---
+++ Lec8/Fun_demo_2.java
@@ -0,0 +1,28 @@
+package Lec8;
+
+public class Fun_demo_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey");
+		System.out.println("by");
+		System.out.println(add());
+
+	}
+	public static int  add() {
+		int a=9;
+		int b=10;
+		int c=a+b;
+		System.out.println(sub());
+		return c;
+	}
+	public static int sub() {
+		int a=9;
+		int b=10;
+		int c=a-b;
+		return c;
+	}
+
+	}
+
+
